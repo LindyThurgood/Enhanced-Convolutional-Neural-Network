f6371880ec3f8cd49ba441048836c271fccd3a34
Lindy Hyde <156486530+LindyThurgood@users.noreply.github.com>
2026-05-11 12:17:00 -0600

    Refactor imports for image augmentation modules
    Signed-off-by: Lindy Hyde <156486530+LindyThurgood@users.noreply.github.com>

--- cnn_models/cnn_ed.py
+++ cnn_models/cnn_ed.py
@@ -13,9 +13,9 @@ from sklearn.metrics import (
 import numpy as np
 import h5py
 from norm_abide import augment_connectivity_matrices
-from new_aug import perform_torchvision_augmentation
+from image_augmentation import perform_torchvision_augmentation
 import torchvision.transforms as T
-from Norm4D import augment_images
+from generic_data_augmentation import augment_images
 
 
 '''
